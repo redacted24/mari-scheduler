222041235aaefde49184fb4bb2402443a852f697
redacted24 <huangq4@outlook.com>
2024-01-05 18:00:22 -0500

    Created components and services for data fetching
    Put fetching backend data into its separate component, backend.js
    Created a React hook (useEffect) to fetch data from server. It is
    currently in a very draft state

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,3 +1,6 @@
+import backendService from './services/backend'
+import { useState, useEffect } from 'react'
+
 const Second = () => {
   console.log("Loaded Second")
   return (
@@ -9,6 +12,28 @@ const Second = () => {
 
 const App = () => {
   console.log("Loaded App")
+
+  const [courses, setCourses] = useState([])
+  const [errorMessage, setErrorMessage] = useState(null)  
+
+  // Initial data fetch
+  useEffect(() => {
+    console.log('Fetching courses data...')
+    backendService
+    .getData()
+    .then((response) => {
+      console.log(response)
+      console.log('✅ Courses data successfully fetched!')
+      setCourses(response)
+    })
+    // Make an Error Message Component
+    .catch(() => {
+      console.log('❌ Courses data could not be fetched')
+      setErrorMessage('Error in fetching country data')
+      setTimeout(() => setErrorMessage(null), 3000)
+    })
+  }, [])
+
   return (
     <div>
       <p>first app</p>
