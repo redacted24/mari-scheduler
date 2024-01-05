f0d7472111ecfd33903a1e62bee409c07726ac18
Crazydodo123 <peizheg@gmail.com>
2024-01-05 16:30:51 -0500

    add parsing logic for schedule

---
+++ data/courses.js
@@ -0,0 +1,32 @@
+const fs = require('node:fs')
+
+const data = fs.readFileSync('./schedule.csv', 'utf-8').split('\r\n')
+data[0] = data[0].slice(1)
+
+
+let courses = []
+let templines = [data[0]]
+
+
+data.slice(1).forEach(line => {
+  if ('1234567890'.includes(line[0])) {
+    const section = templines[0].split(' ')[0]
+    const ID = templines[0].split(' ')[1]
+    const name = templines[0].split(' ').slice(2).join(' ')
+    const teacher = templines[1].slice(1, -1)
+    const schedule = templines.slice(2)
+
+    courses.push({
+      section, ID, name, teacher, schedule
+    })
+
+    templines = [line]
+  } else {
+    templines.push(line)
+  }
+
+})
+
+const jsonString = JSON.stringify(courses, null, 2)
+
+fs.writeFileSync('./schedule.json', jsonString)
